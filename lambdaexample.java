e1e29ce0780416a85c2e8d45661fdc36b0e18241
Birhan_Aschalew <queenbirsh1719@gmail.com>
2023-03-25 08:01:28 -0400

    lambda expression is so precise adn neat .

--- lambdaexample.java
+++ lambdaexample.java
@@ -10,14 +10,14 @@ interface  shape{
     void draw();
 }
 
-
+/*
 class rectangle implements shape{
 
         @Override
         public void draw() {
             System.out.println("rectangle interface implementation");}
-}
-
+}*/
+/*
 class square implements shape{
 
     @Override
@@ -30,11 +30,25 @@ class circle implements shape{
 
     @Override
     public void draw() {
+        System.out.println("circle class implement shape interface");
 
     }
-}
+}*/
 
-
+//lambda expression has only one abstract method, it may has default of static method. and aso it doesn't have return .
 
 public class lambdaexample {
+    public static void main(String[] args) {
+        String str = "hello";
+
+        shape rectangle = ()->System.out.println("rectangle interface implementation");
+        rectangle.draw();
+
+        shape circle = ()->System.out.println("circle class implement shape interface");
+        circle.draw();
+
+        shape square = ()->System.out.println("square implementation from shape interface");
+        square.draw();
+
+    }
 }
